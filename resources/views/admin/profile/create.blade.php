5a9a1610eb0984ead6d3ca920f1593c59b856984
”Shunsuke <sokochi.awscloud9@gmail.com>
2020-09-02 03:55:40 +0000

    kadai001

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>create profile</title>
+</head>
+<body>
+     <h1>ユーザ情報の登録</h1>
+     
+</body>
+</html>
