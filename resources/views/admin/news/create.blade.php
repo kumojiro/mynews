931e1b664ba6150019b557f16b7ed0b1fd879c02
”Shunsuke <sokochi.awscloud9@gmail.com>
2020-09-02 06:09:45 +0000

    kadai011

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -1,12 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>MyNews</title>
-</head>
-<body>
-    <h1>Myニュース作成画面</h1>
-</body>
-</html>
+@extends('layouts.admin')
+
+@section('title','ニュースの新規作成')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md8-8 mx-auto">
+                <h2>ニュース新規作成</h2>
+            </div>
+        </div>
+    </div>
+    @endsection
+
+<!--<!DOCTYPE html>-->
+<!--<html lang="en">-->
+<!--<head>-->
+<!--    <meta charset="UTF-8">-->
+<!--    <meta name="viewport" content="width=device-width, initial-scale=1.0">-->
+<!--    <meta http-equiv="X-UA-Compatible" content="ie=edge">-->
+<!--    <title>MyNews</title>-->
+<!--</head>-->
+<!--<body>-->
+<!--    <h1>Myニュース作成画面</h1>-->
+<!--</body>-->
+<!--</html>-->
---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>@yield('title')</title>
+    <script  src="{{ secure_asset('js/app.js')}}" defer></script>
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link "https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('css/admin.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id ="app">
+        <nav class="navbar navbar-expand-md navbar-dark navbar-laravel">
+            <div class="container">
+                <a class=navbar-brand href="{{ url ('/') }}">
+                    {{ config('app.name', 'Laravel')}}
+                </a>
+                <button class= "navbar-toggler" type="button" data-toggle="collapse" 
+                data-target="#navbarSupportedContent" aria-controls="navbarSupportContent" 
+                aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul clase = "navbar-nav mr-auto">
+                    </ul>
+                    <ul clase = "navbar-nav ml-auto">
+                    </ul>
+                </div>
+            </div>
+            
+        </nav>
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+</body>
+</html>
